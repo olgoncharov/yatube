cb67c09b44aacfc1077120d94eb5fc80267b6149
Oleg Goncharov <oleg.gnch@gmail.com>
2020-01-12 01:38:08 +0300

    add main page of site

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    latest = Post.objects.order_by('-pub_date')[:10]
+    output = []
+    for item in latest:
+        output.append(item.text)
+
+    return HttpResponse('\n'.join(output))
