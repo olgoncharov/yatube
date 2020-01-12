f8e253cd80cf311e3fa0dcd57fae731613a3896f
Oleg Goncharov <oleg.gnch@gmail.com>
2020-01-12 04:33:25 +0300

    add main page to site

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,9 +3,5 @@ from .models import Post
 from django.http import HttpResponse
 
 def index(request):
-    latest = Post.objects.order_by('-pub_date')[:10]
-    output = []
-    for item in latest:
-        output.append(item.text)
-
-    return HttpResponse('\n'.join(output))
+    latest = Post.objects.order_by('-pub_date')[:11]
+    return render(request, 'index.html', {'posts': latest})
