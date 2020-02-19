9799b652897b410bbc6be1ee3e93f0aae69060d7
Oleg Goncharov <oleg.gnch@gmail.com>
2020-02-20 02:20:40 +0300

    1. Add user profile 2. Add post form

--- posts/urls.py
+++ posts/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('new/', views.new_post, name='new_post'),
     path('group/<slug:slug>/', views.group_posts, name='group-posts'),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<username>/', views.profile, name='profile'),
+    path('<username>/<int:post_id>/', views.post_view, name='post'),
+    path('<username>/<int:post_id>/edit/', views.post_edit, name='post_edit')
 ]
--- posts/views.py
+++ posts/views.py
@@ -4,6 +4,10 @@ from .forms import PostForm
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
 
 
 def index(request):
@@ -27,8 +31,53 @@ def new_post(request):
         form = PostForm(request.POST)
         if form.is_valid():
             post = Post.objects.create(author=request.user, **form.cleaned_data)
-            return redirect('index')
-        return render(request, 'new_post.html', {'form': form})
+            return redirect('post', username=request.user.username, post_id=post.pk)
+        return render(request, 'post_edit.html', {'form': form})
 
     form = PostForm()
-    return render(request, 'new_post.html', {'form': form})
+    return render(request, 'post_edit.html', {'form': form})
+
+
+def profile(request, username):
+    context = get_profile_statistics(username)
+    context['paginator'] = Paginator(context['posts'], 5)
+    page_number = request.GET.get('page')
+    context['page'] = context['paginator'].get_page(page_number)
+
+    return render(request, 'profile.html', context)
+
+
+def post_view(request, username, post_id):
+    context = get_profile_statistics(username)
+    context['post'] = get_object_or_404(Post, pk=post_id)
+
+    return render(request, 'post.html', context)
+
+
+def post_edit(request, username, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if post.author != request.user:
+        return redirect('post', username=username, post_id=post_id)
+
+    if request.method == 'POST':
+        if request.POST['button'] == 'Delete':
+            post.delete()
+            return redirect('profile', username=username)
+
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post', username=username, post_id=post_id)
+
+        return render(request, 'post_edit.html', {'form': form})
+
+    return render(request, 'post_edit.html', {'form': PostForm(instance=post)})
+
+
+def get_profile_statistics(username):
+    info = dict()
+    info['owner'] = get_object_or_404(User, username=username)
+    info['posts'] = Post.objects.order_by('-pub_date').filter(author=info['owner'])
+    info['count_of_posts'] = info['posts'].count()
+
+    return info
