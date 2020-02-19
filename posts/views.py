9a358a04849f91b85df4ddf7fb2cda9b4daa731e
Oleg Goncharov <oleg.gnch@gmail.com>
2020-02-19 20:35:02 +0300

    add paginator

--- posts/views.py
+++ posts/views.py
@@ -3,16 +3,23 @@ from .models import Post, Group
 from .forms import PostForm
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 
 def index(request):
-    latest = Post.objects.order_by('-pub_date')[:11]
-    return render(request, 'index.html', {'posts': latest})
+    post_list = Post.objects.order_by('-pub_date').all()
+    paginator = Paginator(post_list, 10)
+
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+    return render(request, 'index.html', {'page': page, 'paginator': paginator})
+
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:12]
     return render(request, 'group.html', {'posts': posts, 'group': group})
+
 
 @login_required
 def new_post(request):
