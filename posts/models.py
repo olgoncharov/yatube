ef89640bfb07efa9be6c6f1f9aa628490ad29842
Oleg Goncharov <oleg.gnch@gmail.com>
2020-03-12 21:08:49 +0300

    increase max length of post text

--- posts/models.py
+++ posts/models.py
@@ -14,7 +14,7 @@ class Group(models.Model):
 
 
 class Post(models.Model):
-    text = models.TextField(max_length=200, verbose_name='Текст')
+    text = models.TextField(max_length=2000, verbose_name='Текст')
     pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts', verbose_name='Автор')
     group = models.ForeignKey(
