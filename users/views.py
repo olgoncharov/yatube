b01e0842f652219bde9dc09ac76ed1c7e185e739
Oleg Goncharov <oleg.gnch@gmail.com>
2020-03-17 23:37:43 +0300

    delete class based controller for signup

--- users/views.py
+++ users/views.py
@@ -7,26 +7,8 @@ from django.core.mail import send_mail
 from .forms import UserForm, UserProfileForm
 
 
-class SignUp(CreateView):
-    """Страница регистрации нового пользователя."""
-    form_class = UserForm
-    success_url = '/auth/login/'
-    template_name = 'signup.html'
-
-    def form_valid(self, form):
-        send_mail(
-            'Подтверждение регистрации',
-            f'Дорогой Вы наш {form.cleaned_data["username"]}, поздравляем Вас с регистрацией',
-            'yatube-noreply@yatube.ru',
-            [form.cleaned_data['email']],
-            fail_silently=False
-        )
-        return super().form_valid(form)
-
-
 def sign_up(request):
     """Страница регистрации нового пользователя."""
-
     if request.method == 'POST':
         user_form = UserForm(request.POST)
         profile_form = UserProfileForm(request.POST, files=request.FILES or None)
